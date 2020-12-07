5462abc0de7f3c4b705c74e06fdf06a0b10f310a
devwilian <wdarioh95@gmail.com>
2020-12-07 13:42:26 -0500

    creacion de la vista formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: FormularioComponent},
+  {path:"**",pathMatch:"full", redirectTo:""},
+  {path:"",pathMatch:"full", redirectTo:""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
